2754df958bf9c7182f58732b777d38d4cc2eeabc
yulekim94 <59432427+yulekim94@users.noreply.github.com>
2020-04-25 16:05:03 +0900

    3차. 완벽구동 CDict 함수화 탑재. (J, K, E)아직

--- Sajeon.py
+++ Sajeon.py
@@ -23,20 +23,48 @@ def definition():
     print(search_receive)
     return jsonify({'result': 'success', 'msg': 'Get이 완료'})
 
-def C_search(a):
+
+def C_search(w):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    url = 'https://dic.daum.net/search.do?q='+ a +'&dic=ch'
+    url = 'https://dic.daum.net/search.do?q='+w+'&dic=ch'
     data = requests.get(url, headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
-    div_meaning = soup.find("div", attrs ={'data-target' : 'mean'})
-    a_link = div_meaning.select_one('div.search_box > div.search_type > div.search_word > strong.tit_searchword >a')
-    href = a_link.get('href')
-    a = href.find('ckw')
-    first_num = href[a+3: a+12]
-    b = href.find('cku')
-    second_num = href[b+3:b+12]
-    definition_url =  'https://dic.daum.net/word/view_supword.do?wordid=ckw'+first_num+'&supid=cku'+second_num+'&suptype=KOREA_CK'
-    print(definition_url)
+    div_word = soup.find("div", attrs ={'data-target': 'word'})
+    if 'tit_cleansch' in str(soup):
+        box_word_bold = div_word.select_one('div.search_box > div.cleanword_type > div.search_cleanword > strong.tit_cleansch > a')
+        href = box_word_bold.get('href')
+        if 'kcw' in href:
+            a = href.find('kcw')
+            first_num = href[a+3:a+12]
+            b = href.find('kcu')
+            second_num = href[b + 3:b + 12]
+            definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kcw' + first_num + '&supid=kcu' + second_num + '&suptype=KOREA_CK'
+            print(definition_url)
+        else:
+            a = href.find('ckw')
+            first_num = href[a + 3: a + 12]
+            b = href.find('cku')
+            second_num = href[b + 3:b + 12]
+            definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=ckw' + first_num + '&supid=cku' + second_num + '&suptype=KOREA_CK'
+            print(definition_url)
+    else:
+        box_word = div_word.select_one('div.search_box > div.search_type > div.search_word > strong.tit_searchword > a')
+        href = box_word.get('href')
+        if 'kcw' in href:
+            a = href.find('kcw')
+            first_num = href[a+3:a+12]
+            b = href.find('kcu')
+            second_num = href[b + 3:b + 12]
+            definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kcw' + first_num + '&supid=kcu' + second_num + '&suptype=KOREA_CK'
+            print(definition_url)
+        else:
+            a = href.find('ckw')
+            first_num = href[a + 3: a + 12]
+            b = href.find('cku')
+            second_num = href[b + 3:b + 12]
+            definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=ckw' + first_num + '&supid=cku' + second_num + '&suptype=KOREA_CK'
+            print(definition_url)
+
 
 def J_search(a):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
