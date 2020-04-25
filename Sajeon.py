355f1f819b672581cd08ba0a571e23b581e0e18c
yulekim94 <59432427+yulekim94@users.noreply.github.com>
2020-04-25 18:13:35 +0900

    3.2 C, J Dict 완성, 함수 저장
    K, E, 그리고 값 없을 시 바로 종료 함수도 만들어야함.

--- Sajeon.py
+++ Sajeon.py
@@ -66,20 +66,48 @@ def C_search(w):
             print(definition_url)
 
 
-def J_search(a):
+def J_search(w):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    url = 'https://dic.daum.net/search.do?q='+a+'&dic=jp'
+    url = 'https://dic.daum.net/search.do?q='+w+'&dic=jp'
     data = requests.get(url, headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
-    div_meaning = soup.find("div", attrs={'data-target' : 'mean'})
-    a_link= div_meaning.select_one('div.search_box > div.search_type > div.search_word > strong.tit_searchword >a')
-    href = a_link.get('href')
-    a = href.find('jkw')
-    first_num = href[a+3: a+12]
-    b = href.find('jku')
-    second_num = href[b+3: b+12]
-    definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=jkw'+first_num+'&supid=jku'+second_num+'&suptype=KUMSUNG_JK'
-    print(definition_url)
+    div_word = soup.find("div", attrs={'class' : 'card_word'})
+    if 'tit_cleansch' in str(soup):
+        box_word_bold = div_word.select_one('div.search_box > div.cleanword_type > div.search_cleanword > strong.tit_cleansch > a')
+        href = box_word_bold.get('href')
+        if 'kjw' in href:
+            a = href.find('kjw')
+            first_num = href[a+3:a+12]
+            b = href.find('kju')
+            second_num = href[b + 3:b + 12]
+            definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kjw' + first_num + '&supid=kju' + second_num + '&suptype=KUMSUNG_KJ'
+            print(definition_url)
+        else:
+            a = href.find('jkw')
+            first_num = href[a + 3: a + 12]
+            b = href.find('jku')
+            second_num = href[b + 3:b + 12]
+            definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=jkw' + first_num + '&supid=jku' + second_num + '&suptype=KUMSUNG_KJ'
+            print(definition_url)
+    else:
+        box_word = div_word.select_one('div.search_box > div.search_type > div.search_word > strong.tit_searchword > a')
+        href = box_word.get('href')
+        if 'kjw' in href:
+            a = href.find('kjw')
+            first_num = href[a+3:a+12]
+            b = href.find('kju')
+            second_num = href[b + 3:b + 12]
+            definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kjw' + first_num + '&supid=kju' + second_num + '&suptype=KUMSUNG_KJ'
+            print(definition_url)
+        else:
+            a = href.find('jkw')
+            first_num = href[a + 3: a + 12]
+            b = href.find('jku')
+            second_num = href[b + 3:b + 12]
+            definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=jkw' + first_num + '&supid=jku' + second_num + '&suptype=KUMSUNG_KJ'
+            print(definition_url)
+
+
 
 def K_search(a):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
