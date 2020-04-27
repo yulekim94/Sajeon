d1c4476fb754a84d7190889de78c0eee411d2d6c
yulekim94 <59432427+yulekim94@users.noreply.github.com>
2020-04-27 12:09:36 +0900

    4. Result 개별 dictinoary값으로 변환

--- definition_crawling.py
+++ definition_crawling.py
@@ -1,44 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
+#
+# def definition(u):
+#     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+#     url = 'u'
+#     data = requests.get(url, headers=headers)
+#     soup = BeautifulSoup(data.text, 'html.parser')
+#     defs = soup.find_all("p", attrs={'class':'desc_item'})
+#     exams = soup.find_all("p", attrs={'class':'desc_ex'})
+#     n=0
+
+    # if 'tit_ex' in str(soup):
+    #     for i in defs:
+    #         if len(i)>0:
+    #             n+=1
+    #             d=i.text.replace("듣기", "").strip()
+    #             print(n,d)
+    #             print('--------------------------------')
+    #     for i in exams:
+    #         e=i.text.replace("듣기", "").strip()
+    #         print(e)
+    # else:
+    #     for i in defs:
+    #         if len(i)>0:
+    #             n+=1
+    #             d=i.text.replace("듣기", "").strip()
+    #             print(n,d)
+    #             print('--------------------------------')
+    #     for i in exams:
+    #         e=i.text.replace("듣기", "").strip()
+    #         print(e)
+
+
+
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 url = 'https://dic.daum.net/word/view_supword.do?wordid=kkw000063427&supid=kku000080607&suptype=KOREA_KK'
 data = requests.get(url, headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
-contents = soup.find_all('p')
 defs = soup.find_all("p", attrs={'class':'desc_item'})
 exams = soup.find_all("p", attrs={'class':'desc_ex'})
-n=0
+#print(defs)
 
-if 'tit_ex' in str(soup):
-    for i in defs:
-        if len(i)>0:
-            n+=1
-            d=i.text.replace("듣기", "").strip()
-            print(n,d)
-            print('--------------------------------')
-    for i in exams:
-        e=i.text.replace("듣기", "").strip()
-        print(e)
-else:
-    for i in defs:
-        if len(i)>0:
-            n+=1
-            d=i.text.replace("듣기", "").strip()
-            print(n,d)
-            print('--------------------------------')
-    for i in exams:
-        e=i.text.replace("듣기", "").strip()
-        print(e)
+for i in defs:
+    if len(i)>0:
+        a=i.text.replace("듣기", "").strip()
+        b= list(a)
+        c="".join(b)
+        d= {'C_def':c}
+        print(d)
 
 
-#
-# if 'tit_ex' in str(soup):
-#     def_t=soup.find("strong", attrs={'class':'tit_ex'}).text
-#     print(def_t)
-#     for i in contents:
-#         a= i.text.replace("듣기", "").strip()
-#         print(a)
-# else:
-#     for i in contents:
-#         a= i.text.replace("듣기", "").strip()
-#         print(a)
+for i in exams:
+    a=i.text.replace("듣기", "").strip()
+    b=list(a)
+    c = "".join(b)
+    d = {'C_exam': c}
+    print(d)
+
