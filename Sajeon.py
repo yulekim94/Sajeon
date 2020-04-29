c4b8438e4c48cd2d1403672d70d4ae29bcebf0ea
yulekim94 <59432427+yulekim94@users.noreply.github.com>
2020-04-29 15:27:24 +0900

    최후의 보루.
    도대체 뭐가 문제인지 모르겠지만 한페이지 검색 업뎃 하고 터짐. 다시 되돌린 버젼

--- Sajeon.py
+++ Sajeon.py
@@ -29,13 +29,17 @@ def definition():
     print(search_receive)
     return jsonify({'result': 'success', 'msg': 'Get이 완료'})
 
+
 def C_search(w):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     url = 'https://dic.daum.net/search.do?q='+w+'&dic=ch'
     data = requests.get(url, headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
     div_word = soup.find("div", attrs ={'data-target': 'word'})
-    if 'card_word' in str(soup):
+    if 'data-target' not in str(soup):
+        No_C ='無 in 中'
+        return No_C
+    else:
         if 'tit_cleansch' in str(soup):
             box_word_bold = div_word.select_one('div.search_box > div.cleanword_type > div.search_cleanword > strong.tit_cleansch > a')
             href = box_word_bold.get('href')
@@ -45,14 +49,14 @@ def C_search(w):
                 b = href.find('kcu')
                 second_num = href[b + 3:b + 12]
                 definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kcw' + first_num + '&supid=kcu' + second_num + '&suptype=KOREA_CK'
-                C_def_results (definition_url)
+                return C_def_results(definition_url)
             else:
                 a = href.find('ckw')
                 first_num = href[a + 3: a + 12]
                 b = href.find('cku')
                 second_num = href[b + 3:b + 12]
                 definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=ckw' + first_num + '&supid=cku' + second_num + '&suptype=KOREA_CK'
-                C_def_results (definition_url)
+                return C_def_results(definition_url)
         else:
             box_word = div_word.select_one('div.search_box > div.search_type > div.search_word > strong.tit_searchword > a')
             href = box_word.get('href')
@@ -62,36 +66,14 @@ def C_search(w):
                 b = href.find('kcu')
                 second_num = href[b + 3:b + 12]
                 definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kcw' + first_num + '&supid=kcu' + second_num + '&suptype=KOREA_CK'
-                C_def_results (definition_url)
+                return C_def_results(definition_url)
             else:
                 a = href.find('ckw')
                 first_num = href[a + 3: a + 12]
                 b = href.find('cku')
                 second_num = href[b + 3:b + 12]
                 definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=ckw' + first_num + '&supid=cku' + second_num + '&suptype=KOREA_CK'
-                C_def_results(definition_url)
-    else:
-        if 'PUBLIC' in str(soup):
-            if 'kcw' in str(soup):
-                a = str(soup.select('meta')[3].get('content'))
-                b = a.find('kcw')
-                first_num = a[b + 3:b + 12]
-                c = a.find('kcu')
-                second_num = a[c + 3:c + 12]
-                definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kcw' + first_num + '&supid=kcu' + second_num + '&suptype=KUMSUNG_CK'
-                C_def_results(definition_url)
-            else:
-                print('b')
-                a = str(soup.select('meta')[3].get('content'))
-                b = a.find('ckw')
-                first_num = a[b + 3:b + 12]
-                c = a.find('cku')
-                second_num = a[c + 3:c + 12]
-                definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kcw' + first_num + '&supid=kcu' + second_num + '&suptype=KUMSUNG_CK'
-                C_def_results(definition_url)
-        else:
-            No_J = '無 in 日'
-            return No_J
+                return C_def_results(definition_url)
 
 
 def J_search(w):
@@ -110,14 +92,14 @@ def J_search(w):
                 b = href.find('kju')
                 second_num = href[b + 3:b + 12]
                 definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kjw' + first_num + '&supid=kju' + second_num + '&suptype=KUMSUNG_KJ'
-                J_def_results (definition_url)
+                return J_def_results(definition_url)
             else:
                 a = href.find('jkw')
                 first_num = href[a + 3: a + 12]
                 b = href.find('jku')
                 second_num = href[b + 3:b + 12]
                 definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=jkw' + first_num + '&supid=jku' + second_num + '&suptype=KUMSUNG_KJ'
-                J_def_results (definition_url)
+                return J_def_results(definition_url)
         else:
             if 'search_box' in str(soup):
                 box_word = div_word.select_one('div.search_box > div.search_type > div.search_word > strong.tit_searchword > a')
@@ -128,43 +110,33 @@ def J_search(w):
                     b = href.find('kju')
                     second_num = href[b + 3:b + 12]
                     definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kjw' + first_num + '&supid=kju' + second_num + '&suptype=KUMSUNG_KJ'
-                    J_def_results(definition_url)
+                    return J_def_results(definition_url)
                 else:
                     a = href.find('jkw')
                     first_num = href[a + 3: a + 12]
                     b = href.find('jku')
                     second_num = href[b + 3:b + 12]
                     definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=jkw' + first_num + '&supid=jku' + second_num + '&suptype=KUMSUNG_KJ'
-                    J_def_results(definition_url)
+                    return J_def_results(definition_url)
             else:
                 a = url.find('kjw')
                 first_num = url[a + 3:a + 12]
                 second_num = soup.find("div", attrs={'class': 'box_word'}).get('data-supid')[3:12]
                 definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kjw' + first_num + '&supid=kju' + second_num + '&suptype=KUMSUNG_KJ'
-                J_def_results(definition_url)
+                return J_def_results(definition_url)
     else:
         if 'PUBLIC' in str(soup):
-            if 'kjw' in str(soup):
+            if 'PUBLIC' in str(soup):
                 a = str(soup.select('meta')[3].get('content'))
                 b = a.find('kjw')
                 first_num = a[b + 3:b + 12]
                 c = a.find('kju')
                 second_num = a[c + 3:c + 12]
                 definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kjw' + first_num + '&supid=kju' + second_num + '&suptype=KUMSUNG_KJ'
-                J_def_results(definition_url)
+                return J_def_results(definition_url)
             else:
-                print('b')
-                a = str(soup.select('meta')[3].get('content'))
-                b = a.find('jkw')
-                first_num = a[b + 3:b + 12]
-                c = a.find('jku')
-                second_num = a[c + 3:c + 12]
-                definition_url = 'https://dic.daum.net/word/view_supword.do?wordid=kjw' + first_num + '&supid=kju' + second_num + '&suptype=KUMSUNG_KJ'
-                J_def_results(definition_url)
-        else:
-            No_J = '無 in 日'
-            return No_J
-
+                No_J = '無 in 日'
+                return No_J
 
 def K_search(w):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
